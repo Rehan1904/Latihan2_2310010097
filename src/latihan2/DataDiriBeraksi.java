58c8679a0190c7dbc15b1b2620840a6f85290bd9
Fukaziroh <rm26277@gmail.com>
2025-06-11 15:37:46 +0800

    Memperbarui kedua class

--- src/latihan2/DataDiriBeraksi.java
+++ src/latihan2/DataDiriBeraksi.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihan2;
 
-/**
- *
- * @author USER
- */
 public class DataDiriBeraksi {
-    
+    public static void main(String[] args) {
+        // Membuat objek dengan data pribadi
+        DataDiri rahma = new DataDiri("Muhammad Rayhan ariadi", "210010097", "4D TI");
+        
+        System.out.println("===== DATA DIRI MAHASISWA =====");
+        rahma.tampilkanData();
+        System.out.println("==============================");
+    }
 }
