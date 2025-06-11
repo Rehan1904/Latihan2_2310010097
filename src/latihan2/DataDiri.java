bb21fbce35fb5be87b91b61a1bc12ffa90806cc7
Fukaziroh <rm26277@gmail.com>
2025-06-11 15:39:04 +0800

    pembaruan

--- src/latihan2/DataDiri.java
+++ src/latihan2/DataDiri.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package latihan2;
 
-/**
- *
- * @author USER
- */
 public class DataDiri {
+    // Atribut
+    private String nama;
+    private String npm;
+    private String kelas;
     
+    // Konstruktor
+    public DataDiri(String nama, String npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+    
+    // Getter dan Setter
+    public String getNama() {
+        return nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNpm() {
+        return npm;
+    }
+    
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+    
+    public String getKelas() {
+        return kelas;
+    }
+    
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+    
+    // Method untuk menampilkan data
+    public void tampilkanData() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NPM: " + npm);
+        System.out.println("Kelas: " + kelas);
+    }
 }
